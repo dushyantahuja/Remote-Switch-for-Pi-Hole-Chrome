f1a0dc0930550750cb1684dd424877c10ea67501
Dushyant Ahuja <dusht.ahuja@gmail.com>
2020-05-29 11:28:40 +0100

    Add default snooze option

--- options/options.js
+++ options/options.js
@@ -1,6 +1,6 @@
 //Function that saves the key to storage
 function setStorage() {
-    chrome.storage.local.set({api_key: document.getElementById("api_key").value, max_time: document.getElementById("max_time").value}, function () {
+    chrome.storage.local.set({api_key: document.getElementById("api_key").value, max_time: document.getElementById("max_time").value}, default_snooze: document.getElementById("default_snooze").value}, function () {
         document.getElementById("confirmation_status").innerHTML = "Saved Successful!";
     });
 }
@@ -10,6 +10,7 @@ function getStorage() {
     chrome.storage.local.get(null, function (data) {
         document.getElementById("api_key").defaultValue = data.api_key;
         document.getElementById("max_time").defaultValue = data.max_time;
+        document.getElementById("default_snooze").defaultValue = data.default_snooze;
     });
 }
 
